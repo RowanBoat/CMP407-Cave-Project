e90c0cf817211dfb6a8b1082cb30eb6fd5307ed1
RowanBoat <56042901+RowanBoat@users.noreply.github.com>
2023-01-03 16:50:11 +0000

    Added Random Pitch to AudioDelayScript

--- Assets/AudioDelayScript.cs
+++ Assets/AudioDelayScript.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class AudioDelayScript : MonoBehaviour
 {
-    public AudioSource source;
+    private AudioSource source;
+    private float waitTimeCountdown = -1f;
+
     public float minWaitBetweenPlays = 1f;
     public float maxWaitBetweenPlays = 5f;
-    public float waitTimeCountdown = -1f;
+    public float minPitch = -3f;
+    public float maxPitch = 3f;
 
     void Start()
     {
@@ -20,6 +23,7 @@ public class AudioDelayScript : MonoBehaviour
         {
             if (waitTimeCountdown < 0f)
             {
+                source.pitch = Random.Range(minPitch, maxPitch);
                 source.Play();
                 waitTimeCountdown = Random.Range(minWaitBetweenPlays, maxWaitBetweenPlays);
             }
